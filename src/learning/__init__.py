2f5c727db0f46a7b522dd8d5edafc6986887aff1
Pavel Sviridov <svipy@yandex-team.ru>
2023-06-29 22:19:46 +0200

    Add admission_learning_started

--- src/learning/__init__.py
+++ src/learning/__init__.py
@@ -1,3 +1,6 @@
+from datetime import date
+from typing import Optional
+
 from django.utils import timezone
 
 from learning.admissions.models import Admission
@@ -38,3 +41,9 @@ def is_user_has_premium_admission(user_id: int, course_id: int) -> bool:
         return False
 
     return admission.is_premium
+
+
+def admission_learning_started(admission_id: int) -> Optional[date]:
+    admission = Admission.objects.get(id=admission_id)
+
+    return admission.squad.start_date if admission.squad else None
--- src/payment/payments/views.py
+++ src/payment/payments/views.py
@@ -3,6 +3,7 @@ from rest_framework import mixins, permissions, serializers, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
+import learning
 from payment.payments.models import Payment, Refund
 
 
@@ -28,7 +29,10 @@ class PaymentViewSet(
         payment = self.get_object()
 
         # Rule #5: user can't create refund when he started to learn.
-        if payment.admission.squad.start_date <= timezone.now().date():
+        if (
+            learning.admission_learning_started(payment.admission_id)
+            <= timezone.now().date()
+        ):
             return Response(
                 status=400, data=dict(error="You have already started to learn.")
             )
