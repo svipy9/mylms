843a263933c0e26e2a613fa8b03d35c42e62f2d8
Pavel Sviridov <svipy@yandex-team.ru>
2023-06-29 23:10:35 +0200

    Refactor grant_premium

--- src/learning/admissions/views.py
+++ src/learning/admissions/views.py
@@ -38,18 +38,12 @@ class ManagerAdmissionSerializer(serializers.ModelSerializer):
         if validated_data.get(
             "is_premium"
         ) and admission.is_premium != validated_data.get("is_premium"):
-            validated_data["paid_at"] = timezone.now()
-            nearest_squad = Squad.objects.filter(
-                course=admission.course,
-                start_date__gt=timezone.now(),
-            ).first()
-            validated_data["squad"] = nearest_squad  # Rule #3
+            admission.grant_premium()  # Rule #3
 
         if not validated_data.get(
             "is_premium"
         ) and admission.is_premium != validated_data.get("is_premium"):
-            validated_data["paid_at"] = None
-            validated_data["squad"] = None
+            admission.revoke_premium()
 
         return super().update(admission, validated_data)
 
